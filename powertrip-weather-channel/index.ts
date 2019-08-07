f3e22ec8d71305adb11a79f37d7f19128f2882dd
Felix Brousseau <felix.brousseau-tremblay.1@live.com>
2019-08-07 22:20:03 +0200

    Remove parsing body

--- powertrip-weather-channel/index.ts
+++ powertrip-weather-channel/index.ts
@@ -6,19 +6,6 @@ const API_KEY = "4afc5e1e71b1a9d4048c5caa0dcba938";
 const BASE_URL = "https://api.darksky.net/forecast/";
 const QUERY_STRING = "exclude=[minutely,hourly,daily,alerts,flags]&units=auto";
 
-const icons = {
-  rain: "rain",
-  snow: "rain",
-  sleet: "cloudy",
-  wind: "cloudy",
-  fog: "cloudy",
-  cloudy: "cloudy",
-  "clear-day": "sunny",
-  "clear-night": "moon",
-  "partly-cloudy-day": "sunny",
-  "partly-cloudy-night": "sunny"
-};
-
 const httpTrigger: AzureFunction = async function(
   context: Context,
   req: HttpRequest
@@ -34,9 +21,7 @@ const httpTrigger: AzureFunction = async function(
   try {
     const url = `${BASE_URL}${API_KEY}/${lat},${long}?${QUERY_STRING}`;
     const { body } = await got(url);
-    const parsed = JSON.parse(body);
-    parsed.currently.icon = icons[parsed.currently.icon];
-    context.res = { body: JSON.stringify(parsed) };
+    context.res = { body };
   } catch (error) {
     context.res = {
       status: 400,
